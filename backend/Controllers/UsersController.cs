4bf0125702e1daa62accdaf1cef440616119df97
Anton Ishchyk <anton.ishchyk@gmail.com>
2026-02-06 21:48:21 +0100

    refactor: add missing model import in UsersController

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Backend.DTOs;
+using Backend.Models;
 using Backend.Services;
 using Google.Apis.Auth;
 
