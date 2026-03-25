1a9b736b8d146e2928d578933b252612cbab8e54
Anton Ishchyk <anton.ishchyk@gmail.com>
2026-03-25 19:40:12 +0100

    fix: correct mapping for OnboardingCompleted in AuthMappingProfile

--- backend/Contracts/Auth/AuthMappingProfile.cs
+++ backend/Contracts/Auth/AuthMappingProfile.cs
@@ -10,7 +10,7 @@ public class AuthMappingProfile : Profile
         // User -> LoginResponse
         CreateMap<User, LoginResponse>()
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.IsOnboardingComplete, opt => opt.MapFrom(src => src.OnboardingCompleted))
+            .ForMember(dest => dest.OnboardingCompleted, opt => opt.MapFrom(src => src.OnboardingCompleted))
             .ForMember(dest => dest.IsAdmin, opt => opt.MapFrom(src => src.IsAdmin))
             .ForMember(dest => dest.Token, opt => opt.Ignore()); // Set manually in service
     }
